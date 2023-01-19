9ad22a1f9dc388c80e9514657adb735610077a5b
Abdulla <abdullasalimov7@gmail.com>
2023-01-19 23:48:17 +0500

    model

--- varah/forms.py
+++ varah/forms.py
@@ -3,5 +3,5 @@ from django import forms
 
 class PriceSearchForm(forms.Form):
         date_from = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-        # date_to = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
+        date_to = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
 
